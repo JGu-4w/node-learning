91d9a2f0d22e054c5685ffbfa65daff361cfab49
Joe Gu <joegu.4work@gmail.com>
2024-08-22 14:19:59 +0800

    feat(koa): add GET/POST query/param/body parser

---
+++ 07_NodeServer_koa/04_koa_request_parse.js
@@ -0,0 +1,46 @@
+const Koa = require('koa');
+const KoaRouter = require('@koa/router');
+const bodyParser = require('koa-bodyparser');
+const multer = require('@koa/multer');
+
+const app = new Koa();
+const formdata = multer();
+app.use(bodyParser());
+
+const userRouter = KoaRouter({ prefix: '/user' });
+
+userRouter.get('/', (ctx, next) => {
+  // query (?attr1=value1&attr2=value2)
+  console.log(ctx.request.query);
+  ctx.body = 'GET method';
+});
+userRouter.get('/:id', (ctx, next) => {
+  // params (/:id)
+  console.log(ctx.request.params);
+  ctx.body = 'GET method';
+});
+
+// POST 请求中的 body 需要通过第三方插件 koa-bodyparser 解析
+// application/json
+userRouter.post('/json', (ctx, next) => {
+  console.log(ctx.request.body);
+  ctx.body = 'json body ' + JSON.stringify(ctx.request.body);
+});
+// x-www-form-urlencode
+userRouter.post('/urlencode', (ctx, next) => {
+  console.log(ctx.request.body);
+  ctx.body = 'urlencode body ' + JSON.stringify(ctx.request.body);
+});
+
+// POST 请求中的 form-data 格式需要使用 @koa/multer 解析
+// form-data
+userRouter.post('/formdata', formdata.any(), (ctx, next) => {
+  console.log(ctx.request.body);
+  ctx.body = 'formdata body ' + JSON.stringify(ctx.request.body);
+});
+
+app.use(userRouter.routes());
+
+app.listen(8000, () => {
+  console.log('Start Listening...');
+});
